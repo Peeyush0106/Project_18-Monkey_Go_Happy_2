1d8adc35cae22b9f147bab714c99d905403e93b0
Peeyush0106 <69502115+Peeyush0106@users.noreply.github.com>
2020-10-12 19:45:33 +0530

    Add files via upload

---
+++ Project 18/sketch.js
@@ -0,0 +1,36 @@
+//Declare all global variables
+var ground, monkeyHand, reset,
+    monkey, monkeyAutomatedCollider, gameState,
+    monkeyAutomatedColliderMonkeyXAddNumber,
+    waitageTime, score, time, PLAY, END, stones, bananas, singlePlayerButton, automatedPlayingButton;
+
+var groundImage, monkeyHandImage, resetImage, monkeyImage, singlePlayerButtonImage, automatedPlayerButtonImage, bananaImage, stoneImage, monkeyJumpImage;
+
+function preload() {
+    groundImage = loadImage("ground.png");
+    monkeyHandImage = loadImage("monkey_jump_hand.png");
+    resetImage = loadImage("reset.png");
+    monkeyImage = loadAnimation("Monkey_01.png", "Monkey_02.png", "Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_09.png", "Monkey_10.png");
+    singlePlayerButtonImage = loadImage("single_player.png");
+    automatedPlayerButtonImage = loadImage("automated_gaming_mode.png");
+    bananaImage = loadImage("banana.png");
+    stoneImage = loadImage("stone.png");
+    monkeyJumpImage = loadImage("monkey_jump.png");
+}
+
+function setup() {
+    createCanvas(400, 400);
+    // The setup for all the globally defined variables and other things
+    ground = createSprite(200, 325);
+    ground.displayWidth(1600);
+    ground.addImage("ground", groundImage);
+    ground.y = 400 - (ground.height / 2);
+}
+
+function draw() {
+    background("lightblue");
+    //External functions
+    // Draw the sprites
+    drawSprites();
+}
+
